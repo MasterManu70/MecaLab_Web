05002dff2d5cbe1a54867d9071614f1986f0a7e0
TheSincarS <jesus_fco_981@hotmail.com>
2020-08-09 20:20:17 -0500

    [MOD] Style.css Responsividad mejorada

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+//Declarar variable
 var temp = document.getElementById("buscar").value;
 
+//Ocultar el menu del clic derecho
 document.oncontextmenu = function () {
     return false;
 }
 
+//Generar cookie y sobreescribirlo cada 10 segundos
 $(document).ready(function () {
     setInterval(function () {
 
